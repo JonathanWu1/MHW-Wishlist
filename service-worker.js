c4a08c95c51784405da10aff995c62458c6e5555
jonathanwu1 <jonathan.wu@corefactors.in>
2019-12-12 19:45:08 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/!.github.io/MHW-Wishlist/precache-manifest.3482a4778bbd8518d92bc739d48b16b4.js"
+  "/MHW-Wishlist/precache-manifest.19ee827995efe50ae58509eebf2d9721.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/!.github.io/MHW-Wishlist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MHW-Wishlist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
