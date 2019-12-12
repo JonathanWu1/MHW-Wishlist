b375b487102fea4761e10a8f7115396651302b3e
jonathanwu1 <jonathan.wu@corefactors.in>
2019-12-12 19:32:13 +0530

    Updates

---
+++ precache-manifest.8496bac8bc342a20e195bc4e033b89dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65a5469bcae78081609e61d688245be",
+    "url": "/!.github.io/MHW-Wishlist/index.html"
+  },
+  {
+    "revision": "461d301177e19ff342c7",
+    "url": "/!.github.io/MHW-Wishlist/static/css/main.b9709a22.chunk.css"
+  },
+  {
+    "revision": "61eec1f20a0887b48764",
+    "url": "/!.github.io/MHW-Wishlist/static/js/2.93a96bab.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/!.github.io/MHW-Wishlist/static/js/2.93a96bab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "461d301177e19ff342c7",
+    "url": "/!.github.io/MHW-Wishlist/static/js/main.30b20746.chunk.js"
+  },
+  {
+    "revision": "e37a36c4055b444fd7a2",
+    "url": "/!.github.io/MHW-Wishlist/static/js/runtime-main.f35c551d.js"
+  }
+]);
